610ea86d17a872f6c956144cff93a536cb62e7d7
yrnoh <yrnoh@rsupport.com>
2025-12-12 11:46:19 +0900

    chore: TanStack Query, Zustand 등 의존성 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/front_7th_chapter3-3/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
