38105a60658f92c05b8fd93d12ef06efdc825cc9
yrnoh <yrnoh@rsupport.com>
2025-12-12 11:53:57 +0900

    refactor: 기존 컴포넌트 구조 제거 및 진입점 수정
    - 기존 components 폴더 삭제
    - App.tsx, main.tsx FSD 구조에 맞게 수정
    - tsconfig.app.json 경로 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./components/Header.tsx"
-import Footer from "./components/Footer.tsx"
-import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Header } from './widgets/layout/ui/Header'
+import { Footer } from './widgets/layout/ui/Footer'
+import { PostsManagerPage } from './pages/PostsManagerPage'
+import { QueryProvider } from './app/providers'
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <QueryProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </QueryProvider>
   )
 }
 
